2a7d7d3ff492fe41e1ee103ef5f598b44c8aa9d1
Tal137 <tal1376521@gmail.com>
2024-05-05 22:54:02 +0300

    Update StudentController.java

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Random;
-@CrossOrigin
+@CrossOrigin(origins = "https://class-student.netlify.app")
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
